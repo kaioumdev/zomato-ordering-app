9501b159b5de05b824360cf7dc07da3303d93386
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-06 04:13:56 +0600

    feat(body): create body component successfully

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,17 @@ const Header = () => {
   )
 }
 
+const Body = () => {
+  return (
+    <div className='body'>
+      <div className="search">
+        Search
+      </div>
+      <div className="res-container"></div>
+    </div>
+  )
+}
+
 function App() {
 
   return (
