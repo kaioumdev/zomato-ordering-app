077c07caa8c85841756fba12dec2e385e8a47f25
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-06 05:48:45 +0600

    feat(restaurantCard); complete restaurantCard items successfully with styles

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,12 @@ const Header = () => {
 
 const RestaurantCard = () => {
   return (
-    <div className="res-card">
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0f0" }}>
+      <img className='res-logo' src="https://b.zmtcdn.com/data/dish_photos/91a/f5d03c4d5534a7d7f05a127e053a191a.png" alt="res-logo" />
       <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian</h4>
+      <h4>4.4 stars</h4>
+      <h4>48 minutes</h4>
     </div>
   )
 }
@@ -46,6 +50,7 @@ function App() {
   return (
     <div className='app'>
       <Header></Header>
+      <Body></Body>
     </div>
   )
 }
