3b2e070764381a92c46a7e16556b1d12d7085632
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-06 23:02:21 +0600

    feat(Image): show single image dynamically from resData

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,12 @@ const RestaurantCard = (props) => {
   const { resData } = props;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0f0" }}>
-      <img className='res-logo' src={"https://www.swiggy.com/collections/" + resData?.imageId} alt="res-logo" />
+      <img className='res-logo' src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData?.info.cloudinaryImageId} alt="res-logo" />
       <h3>{resData?.info.name}</h3>
-      <h4>{resData?.info.cuisines.join(" , ")}</h4>
-      <h4>4.4 stars</h4>
-      <h4>48 minutes</h4>
+      <h4>{resData?.info.cuisines.join(", ")}</h4>
+      <h4>{resData.info.avgRating}</h4>
+      <h4>{resData.info.costForTwo}</h4>
+      <h4>{resData.info.sla.deliveryTime} Minutes</h4>
     </div>
   )
 }
