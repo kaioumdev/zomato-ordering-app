bd30eb9ee659de020043ad1027a73dcc4d3d16e3
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-07 14:51:40 +0600

    feat(Header): create new Header component successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
 import './App.css'
-
-const Header = () => {
-  return (
-    <div className='header'>
-      <div className='logo-container'>
-        <img className='logo' src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png' alt='logo' />
-      </div>
-      <div>
-        <div className='nav-items'>
-          <ul className='nav-items'>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
-}
+import { Header } from './components/Header';
 
 const RestaurantCard = (props) => {
   const { resData } = props;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png' alt='logo' />
+            </div>
+            <div>
+                <div className='nav-items'>
+                    <ul className='nav-items'>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                        <li>Cart</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
