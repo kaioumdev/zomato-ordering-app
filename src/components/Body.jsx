f10376ec01e394e805e1e5d069077cf42f90d48f
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-07 22:18:16 +0600

    feat(filterList): filter restaurantList by using avgRating

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
 import resLists from "../utils/mocData";
 import RestaurantCard from "./RestaurantCard";
 
 
 const Body = () => {
+
     const filteredRestaurants = resLists
         .map((item) => item.card?.card)
         .filter((card) => card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.Restaurant");
+    const [lisOfRestaurants, setListOfRestaurants] = useState(filteredRestaurants);
+
 
     return (
         <div className='body'>
-            <div className="search">
-                Search
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const filteredList = lisOfRestaurants.filter((res) => res?.info?.avgRating > 4)
+                    setListOfRestaurants(filteredList)
+                }}>Top Rated Restaurant</button>
             </div>
             <div className="res-container">
                 {
-                    filteredRestaurants?.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
+                    lisOfRestaurants?.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
                 }
             </div>
         </div>
