3a82b49f9e07d208c4eb3d7a39656bf06dbec223
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-09 15:11:36 +0600

    feat(errorPage): create an errorPage successully and connect with by react-router-dom successfully

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,22 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import Error from './components/Error.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <Error></Error>
   },
   {
     path: "/about",
     element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
   }
 ])
 
