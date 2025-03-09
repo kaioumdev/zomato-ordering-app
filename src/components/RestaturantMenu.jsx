3abc133eff9e47f90604493356e022676dc36489
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-09 18:53:58 +0600

    feat(singleData): successfully fetch the singleData from API & show the card item

--- src/components/RestaturantMenu.jsx
+++ src/components/RestaturantMenu.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Shimmer from './Shimmer'
 
 const RestaturantMenu = () => {
+    const [resInfo, setResInfo] = useState(null)
     useEffect(() => {
         fetchMenu()
     }, [])
@@ -8,10 +10,13 @@ const RestaturantMenu = () => {
     const fetchMenu = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=16866&catalog_qa=undefined&submitAction=ENTER")
         const res = await data.json()
+        setResInfo(res.data)
         console.log(res)
     }
-    return (
-        <div>RestaturantMenu</div>
+    return resInfo === null ? (<Shimmer></Shimmer>) : (
+        <div>
+            <h1>{resInfo?.cards[0]?.card?.card?.text}</h1>
+        </div>
     )
 }
 
