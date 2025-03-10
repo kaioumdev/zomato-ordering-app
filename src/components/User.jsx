073a73ecce32a8626f2823387d01f265bdecc7ac
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-10 15:17:10 +0600

    feat(userClass): create new UserClass calss based component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const User = () => {
     return (
-        <div>User</div>
+        <div className="user-card">
+            <h2>Name: Kaioum</h2>
+            <h3>Location: Bangladesh</h3>
+            <h4>Email: kaiou@gmail.com</h4>
+        </div>
     )
 }
 
---
+++ src/components/UserClass.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+class UserClass extends React.Component {
+    render() {
+        return <div className="user-card">
+            <h2>Name: Kaioum</h2>
+            <h3>Location: Bangladesh</h3>
+            <h4>Email: kaioum@gmail.com</h4>
+        </div>;
+    }
+}
