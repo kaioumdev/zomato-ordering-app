68ff7dbaa44e028b681cc97926de28826f95f4fa
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-12 17:24:59 +0600

    feat(useRestaurantMenu): create useRestaurantMenu hooks successfully

---
+++ src/utils/useRestaurantMenu.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const useRestaurantMenu = () => {
+    const [resInfo, setResInfo] = useState(null);
+
+
+    return resInfo;
+}
+
+export default useRestaurantMenu
