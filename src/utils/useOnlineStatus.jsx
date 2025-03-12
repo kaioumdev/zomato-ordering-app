899f57e5d48413fa3f5221a3fdf1440faa216c01
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-12 21:51:03 +0600

    feat(onlineStatus): set initialStatus true

--- src/utils/useOnlineStatus.jsx
+++ src/utils/useOnlineStatus.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 const useOnlineStatus = () => {
-    const [onlineStatus, setOnlineStatus] = useState();
+    const [onlineStatus, setOnlineStatus] = useState(true);
 
     useEffect(() => {
         window.addEventListener("offline", () => {
