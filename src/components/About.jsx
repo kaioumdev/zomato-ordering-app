03ee905bd446e931ec4ef96ea81e98a59bc8c0f1
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-12 06:50:48 +0600

    feat(state): updated userInfo using state successfully

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,6 @@ const About = () => {
         <div>
             <h1>About</h1>
             <p>This is the about page.</p>
-            <User></User>
             <UserClass name="Kaiyum"></UserClass>
         </div>
     )
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
-        console.log(props)
+
+        this.state = {
+            userInfo: {
+                name: "Dummy User",
+                location: "default"
+            }
+        }
 
     }
 
     async componentDidMount() {
         const data = await fetch("https://api.github.com/users/kaioumdev")
         const json = await data.json()
+        this.setState({ userInfo: json })
         console.log(json)
     }
     render() {
+        const { name, location } = this.state.userInfo;
         return <div className="user-card">
             {/* <button onClick={() => {
                 //NEVER EVER UPDATE STATE VARIABLE DIRECTLY
                 // this.state.count = this.state.count + 1
                 this.setState({ count: this.state.count + 1 })
             }}>Count Increase</button> */}
-            <h2>Name: {this.props.name}</h2>
-            <h3>Location: Bangladesh</h3>
+            <h2>Name: {name}</h2>
+            <h3>Location: {location}</h3>
             <h4>Email: kaioum@gmail.com</h4>
         </div>;
     }
