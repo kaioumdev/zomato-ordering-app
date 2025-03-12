d6076502a31c7dcfd96daabffabf9d72c9f4dd20
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-12 06:40:59 +0600

    feat(API): successfully make API call by using componentDidMount method

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -4,20 +4,20 @@ class UserClass extends React.Component {
         super(props);
         console.log(props)
 
-        this.state = {
-            count: 0,
-            count2: 0,
-        }
+    }
+
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/kaioumdev")
+        const json = await data.json()
+        console.log(json)
     }
     render() {
-        const { count } = this.state
         return <div className="user-card">
-            <h1>Count: {count}</h1>
-            <button onClick={() => {
+            {/* <button onClick={() => {
                 //NEVER EVER UPDATE STATE VARIABLE DIRECTLY
                 // this.state.count = this.state.count + 1
                 this.setState({ count: this.state.count + 1 })
-            }}>Count Increase</button>
+            }}>Count Increase</button> */}
             <h2>Name: {this.props.name}</h2>
             <h3>Location: Bangladesh</h3>
             <h4>Email: kaioum@gmail.com</h4>
