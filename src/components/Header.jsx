44cb0e5f9a7edfb96ef7fc6abff9552f3e730b11
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-13 14:37:16 +0600

    feat(header): complete header component styles by using tailwindcss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,29 +7,29 @@ const Header = () => {
     const [btnNameReact, setBtnNameReact] = useState('Login');
     const onlineStatus = useOnlineStatus();
     return (
-        <div className='header'>
+        <div className='flex justify-between bg-pink-100 shodow-lg'>
             <div className='logo-container'>
                 <Link to="/">
-                    <img className='logo' src={LOGO_URL} alt='logo' />
+                    <img className='w-36' src={LOGO_URL} alt='logo' />
                 </Link>
             </div>
             <div>
-                <div className='nav-items'>
-                    <ul className='nav-items'>
-                        <li>Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
-                        <li>
+                <div className='flex items-center'>
+                    <ul className='flex p-4 m-4'>
+                        <li className="px-4">Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+                        <li className="px-4">
                             <Link to="/">Home</Link>
                         </li>
-                        <li>
+                        <li className="px-4">
                             <Link to="/about">About Us</Link>
                         </li>
-                        <li>
+                        <li className="px-4">
                             <Link to="/contact">Contact Us</Link>
                         </li>
-                        <li>
+                        <li className="px-4">
                             <Link to="/grocery">Grocery</Link>
                         </li>
-                        <li>Cart</li>
+                        <li className="px-4">Cart</li>
                         <button className="login" onClick={() => btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login")}>{btnNameReact}</button>
                     </ul>
                 </div>
