4883b18a5c9e13f171c9147bd843fc9cd5d80ed5
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-14 05:48:16 +0600

    feat(ItemList): create new ItemList component

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ItemList from './ItemList'
 
 const RestaurantCategory = ({ data }) => {
     return (
-        <div className='w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between'>
-            <span>{data?.title} ({data?.itemCards.length})</span>
+        <div>
+            <div className='w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between'>
+                <span>{data?.title} ({data?.itemCards.length})</span>
+                <span>⬇️</span>
+            </div>
+            {/* accordian body */}
+            <ItemList></ItemList>
         </div>
     )
 }
