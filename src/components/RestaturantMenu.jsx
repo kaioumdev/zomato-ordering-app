0448c879ca4c83503457d92329fd4ca516ff2f4d
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-14 03:52:09 +0600

    feat(categories): got cagetories itemsb by using filter method

--- src/components/RestaturantMenu.jsx
+++ src/components/RestaturantMenu.jsx
@@ -16,20 +16,23 @@ const RestaturantMenu = () => {
     const { name, cuisines, costForTwoMessage } = restaurantInfo.card?.card?.info || {};
 
     const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-    console.log(itemCards)
+    console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
+
+    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
+    console.log(categories)
 
     return (
         <div className='menu'>
             <h1>{name || "Restaurant Name Not Available"}</h1>
             <h3>{cuisines?.join(", ")} - {costForTwoMessage}</h3>
-            <h2>Menu</h2>
+            {/* <h2>Menu</h2>
             <ul>
                 {
                     itemCards?.map((item) => (
                         <li key={item?.card?.info?.id}>{item?.card?.info?.name} - {"$"} {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}</li>
                     ))
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
