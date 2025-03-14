53d013c97110a4019e18340f4c9fd86da3eba137
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-14 06:13:46 +0600

    feat(description): successfully show the defaultPrice & description at accordian

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const ItemList = () => {
+const ItemList = ({ items }) => {
     return (
-        <div>ItemList</div>
+        <div>
+            {
+                items.map((item) => (
+                    <div key={item.card.info.id} className='p-2 m-2 bg-gray-200 border-b-2 text-left'>
+                        <div className='py-2'>
+                            <span>{item?.card?.info?.name}</span>
+                            <span> - $ {item?.card?.info?.price ? item?.card?.info?.price / 100 : item?.card?.info?.defaultPrice / 100}</span>
+                        </div>
+                        <p className='text-xs'>{item?.card?.info?.description}</p>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -4,12 +4,14 @@ import ItemList from './ItemList'
 const RestaurantCategory = ({ data }) => {
     return (
         <div>
-            <div className='w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between'>
-                <span>{data?.title} ({data?.itemCards.length})</span>
-                <span>⬇️</span>
+            <div className='w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4'>
+                <div className='flex justify-between'>
+                    <span>{data?.title} ({data?.itemCards.length})</span>
+                    <span>⬇️</span>
+                </div>
             </div>
             {/* accordian body */}
-            <ItemList></ItemList>
+            <ItemList items={data?.itemCards}></ItemList>
         </div>
     )
 }
