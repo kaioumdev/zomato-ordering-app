d2164912f61fd00fa4dc51325775edce65c967f9
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-15 02:58:03 +0600

    feat(ItemList): complete ItemList styles successfully

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -6,13 +6,20 @@ const ItemList = ({ items }) => {
         <div>
             {
                 items.map((item) => (
-                    <div key={item.card.info.id} className='p-2 m-2 bg-gray-200 border-b-2 text-left'>
-                        <img src={CDN_URL + item.card.info.imageId} alt="" className='w-14' />
-                        <div className='py-2'>
-                            <span>{item?.card?.info?.name}</span>
-                            <span> - $ {item?.card?.info?.price ? item?.card?.info?.price / 100 : item?.card?.info?.defaultPrice / 100}</span>
+                    <div key={item.card.info.id} className='p-2 m-2 bg-gray-200 border-b-2 text-left flex justify-between'>
+                        <div className='w-9/12'>
+                            <div className='py-2'>
+                                <span>{item?.card?.info?.name}</span>
+                                <span> - $ {item?.card?.info?.price ? item?.card?.info?.price / 100 : item?.card?.info?.defaultPrice / 100}</span>
+                            </div>
+                            <p className='text-xs'>{item?.card?.info?.description}</p>
                         </div>
-                        <p className='text-xs'>{item?.card?.info?.description}</p>
+                        <div className='w-3/12 p-4'>
+                            <div className='absolute'>
+                                <button className='p-2 mx-16 rounded-lg text-white bg-black shadow-lg m-auto'>Add +</button>
+                            </div>
+                            <img src={CDN_URL + item.card.info.imageId} alt="" className='w-full' />
+                        </div>
                     </div>
                 ))
             }
