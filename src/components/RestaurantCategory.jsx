cf64352b72bd79772cb102f06c74699ffe1ea22e
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-15 03:01:36 +0600

    feat(restaurantCategory): solve the restaturantCategory styles successfully

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -6,12 +6,12 @@ const RestaurantCategory = ({ data }) => {
         <div>
             <div className='w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4'>
                 <div className='flex justify-between'>
-                    <span>{data?.title} ({data?.itemCards.length})</span>
+                    <span className='font-bold text-lg'>{data?.title} ({data?.itemCards.length})</span>
                     <span>⬇️</span>
                 </div>
+                {/* accordian body */}
+                <ItemList items={data?.itemCards}></ItemList>
             </div>
-            {/* accordian body */}
-            <ItemList items={data?.itemCards}></ItemList>
         </div>
     )
 }
