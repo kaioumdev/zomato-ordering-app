e1ded4c158b70607e25a386fe0506255a2cc6605
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-17 03:44:15 +0600

    feat(clearCart): clearCard reducer work successfully to clear all cartData

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import ItemList from './ItemList'
+import { clearCart } from '../utils/cartSlice';
 
 const Cart = () => {
-    const cartItems = useSelector((state) => state.cart.items)
+    const cartItems = useSelector((state) => state.cart.items);
+    const dispatch = useDispatch()
+    const handleClearCart = () => {
+        dispatch(clearCart())
+    }
     return (
         <div className='text-center m-4 p-4'>
             <h1 className='text-2xl font-bold'>Cart</h1>
             <div className='w-6/12 m-auto'>
+                <button onClick={handleClearCart} className='p-2 m-2 bg-black text-white rounded-lg'>Clear Cart</button>
                 <ItemList items={cartItems}></ItemList>
             </div>
         </div>
