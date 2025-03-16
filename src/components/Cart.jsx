5bcc250533afbc53a91d7f5347d9dfc3ea8b062e
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-17 03:16:27 +0600

    feat(cartItems): show cartItems to the cart component successfully

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import ItemList from './ItemList'
 
 const Cart = () => {
+    const cartItems = useSelector((state) => state.cart.items)
     return (
-        <div>Cart</div>
+        <div className='text-center m-4 p-4'>
+            <h1 className='text-2xl font-bold'>Cart</h1>
+            <div>
+                <ItemList items={cartItems}></ItemList>
+            </div>
+        </div>
     )
 }
 
