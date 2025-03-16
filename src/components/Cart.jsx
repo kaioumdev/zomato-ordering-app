3eb869d675ce973d9224a8cab728190793d294db
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-17 03:36:12 +0600

    got the cart item to purches to add cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,7 +7,7 @@ const Cart = () => {
     return (
         <div className='text-center m-4 p-4'>
             <h1 className='text-2xl font-bold'>Cart</h1>
-            <div>
+            <div className='w-6/12 m-auto'>
                 <ItemList items={cartItems}></ItemList>
             </div>
         </div>
