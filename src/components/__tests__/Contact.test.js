8633dc6c02db066decdf33eb7557541210513953
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-19 14:23:32 +0600

    feat(describe): describe conatact us page test case

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,42 +2,46 @@ import { render, screen } from "@testing-library/react"
 import Contact from "../Contact"
 import "@testing-library/jest-dom"
 /* eslint-disable no-undef */
-test("Should load contact us component", () => {
-    render(<Contact />)
 
-    const heading = screen.getByRole('heading')
+describe("Contact component", () => {
+    test("Should load contact us component", () => {
+        render(<Contact />)
 
-    expect(heading).toBeInTheDocument()
+        const heading = screen.getByRole('heading')
+
+        expect(heading).toBeInTheDocument()
+    })
+
+    test("Should load button inside contact component", () => {
+        render(<Contact />)
+
+        const button = screen.getByRole('button')
+
+        expect(button).toBeInTheDocument()
+    })
+
+    test("Should load button inside contact component", () => {
+        render(<Contact />)
+
+        const button = screen.getByText('Submit')
+
+        expect(button).toBeInTheDocument()
+    })
+
+    test("Should load input name inside contact component", () => {
+        render(<Contact />)
+
+        const input = screen.getByPlaceholderText('Name')
+
+        expect(input).toBeInTheDocument()
+    })
+
+    test("Should load input element inside contact component", () => {
+        render(<Contact />)
+
+        const inputBoxes = screen.getAllByRole('textbox')
+
+        expect(inputBoxes.length).toBe(2)
+    })
 })
 
-test("Should load button inside contact component", () => {
-    render(<Contact />)
-
-    const button = screen.getByRole('button')
-
-    expect(button).toBeInTheDocument()
-})
-
-test("Should load button inside contact component", () => {
-    render(<Contact />)
-
-    const button = screen.getByText('Submit')
-
-    expect(button).toBeInTheDocument()
-})
-
-test("Should load input name inside contact component", () => {
-    render(<Contact />)
-
-    const input = screen.getByPlaceholderText('Name')
-
-    expect(input).toBeInTheDocument()
-})
-
-test("Should load input element inside contact component", () => {
-    render(<Contact />)
-
-    const inputBoxes = screen.getAllByRole('textbox')
-
-    expect(inputBoxes.length).toBe(2)
-})
