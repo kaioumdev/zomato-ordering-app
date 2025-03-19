1a4a77d8d37beb1769c7b92c6622e00a3db453d4
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-20 03:21:34 +0600

    feat(mockRestListData): create mockResListData & fetch this data at search.test.js file

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-undef */
 import { render } from "@testing-library/react"
 import Body from "../Body"
+import MOCK_DATA from "../../components/mocks/mockResListsData.json"
 
 global.fetch = jest.fn(() => {
     return Promise.resolve({
         json: () => {
-            return Promise.resolve(data)
+            return Promise.resolve(MOCK_DATA)
         }
     })
 }
