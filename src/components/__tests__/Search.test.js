b52b2d30f83667b6bb8d6f8b3aefea5c5a6af039
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-20 03:47:08 +0600

    feat(searchBtn): searchBtn pass test cases successfully

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Body from "../Body"
 import MOCK_DATA from "../../components/mocks/mockResListsData.json"
 // import { act } from "react"
 import { act } from "react-dom/test-utils"
 import { BrowserRouter } from "react-router-dom"
+import "@testing-library/jest-dom"
 
 global.fetch = jest.fn(() => {
     return Promise.resolve({
@@ -17,4 +18,8 @@ global.fetch = jest.fn(() => {
 /* eslint-disable no-undef */
 test("Should render the Body component with search", async () => {
     await act(async () => render(<BrowserRouter><Body></Body></BrowserRouter>))
+
+    const searchBtn = screen.getByRole('button', { name: 'Search' })
+    console.log(searchBtn)
+    expect(searchBtn).toBeInTheDocument()
 })
