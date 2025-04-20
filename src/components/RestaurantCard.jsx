eca6243cd3328cb44483849699fe7698091cbb74
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-20 16:51:08 +0600

    got resData successfully to remove resData console from restaurantCard

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -4,7 +4,6 @@ import { CDN_URL } from "../utils/contants";
 
 const RestaurantCard = (props) => {
     const { resData } = props;
-    console.log(resData)
     return (
         <div data-testid="resCard" className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
             <img className='res-logo rounded-lg' src={CDN_URL + resData?.info?.cloudinaryImageId} alt="res-logo" />
